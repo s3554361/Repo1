3bb2a5824ebae06c232ebb43232e7988da057b47
s3554361 <s3554361@students.fscj.edu>
2022-11-13 10:34:22 -0800

    Netmiko commit

---
+++ netmiko.py
@@ -0,0 +1,14 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+USERNAME = input("Please enter your SSH username: ")
+PASS = getpass("Please enter your SSH password: ")
+
+device = {
+    'ip': {'192.168.1.10'},
+    'username': USERNAME,
+    'password': PASS,
+    'device_type': 'cisco_ios'
+}
+
+c = ConnectHandler(**device)
