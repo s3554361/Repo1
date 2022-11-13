676a47cd2dfad35949f62f1e333d6e9885958389
s3554361 <s3554361@students.fscj.edu>
2022-11-13 11:30:08 -0800

    Updated SSH file and created requirements

--- SSHCisco.py
+++ SSHCisco.py
@@ -1,6 +1,7 @@
 import os
 from netmiko import ConnectHandler
 from getpass import getpass
+from netmiko.ssh_exception import AuthentionException, ssh_exception, NetMikoTimeoutException
 
 USERNAME = input("Please enter your SSH username: ")
 PASS = getpass("Please enter your SSH password: ")
@@ -12,11 +13,15 @@ device = {
     'device_type': 'cisco_ios'
 }
 
-c = ConnectHandler(**device)
-
-output = c.send_command('show run')
-
-f = open('backup.conf', 'x')
-
-f.write(output)
-f.close()
+try:
+    c = ConnectHandler(**device)
+    output = c.send_command('show run')
+    f = open('backup.conf', 'x')
+    f.write(output)
+    f.close()
+except (AuthentionException):
+    print("An aunthentication error occured while connecting to:" + device[ip])
+except (ssh_exception):
+    print("An error occured while connecting to device " + device[ip] + "via ssh. Is SSH enabled?")
+except (NetMikoTimeoutException):
+    print("The device " = device[ip] + " timed out when attempting to connect")
